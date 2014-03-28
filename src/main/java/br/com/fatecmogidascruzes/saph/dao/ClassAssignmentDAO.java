2c71853ab67ada176ff55fa1f6edce272d409dd6
Marcelo Birche Ferri <marcelobirche@gmail.com>
2014-03-27 22:40:48 -0300

    Continuação

--- src/main/java/br/com/fatecmogidascruzes/saph/dao/ClassAssignmentDAO.java
+++ src/main/java/br/com/fatecmogidascruzes/saph/dao/ClassAssignmentDAO.java
@@ -1,10 +1,12 @@
 package br.com.fatecmogidascruzes.saph.dao;
 
+import br.com.fatecmogidascruzes.saph.interfaces.IClassAssignmentFacade;
+
 /**
  * All fonts were produced for author's graduation project
  * @date 27/03/2014
  * @author Birche
  */
-public class ClassAssignmentDAO extends AbstractDAO{
+public class ClassAssignmentDAO extends AbstractDAO implements IClassAssignmentFacade{
 
 }
