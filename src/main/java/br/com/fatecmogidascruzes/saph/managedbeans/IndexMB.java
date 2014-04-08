4bc6f8f5ac4f0300686a68460203725233e75639
Marcelo Birche Ferri <marcelobirche@gmail.com>
2014-04-07 23:51:39 -0300

    Tentando fazer o primefaces funcionar

---
+++ src/main/java/br/com/fatecmogidascruzes/saph/managedbeans/IndexMB.java
@@ -0,0 +1,17 @@
+package br.com.fatecmogidascruzes.saph.managedbeans;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * All fonts were produced for author's graduation project
+ * @date 07/04/2014
+ * @author Birche
+ */
+
+@ManagedBean(name="indexMB")
+@SessionScoped
+public class IndexMB {
+
+}
